acf49345049f95de881da19d6ddc6f59ad500087
Anton Rusak <anton.rusak@perfectart.com>
2025-07-23 11:18:11 +0300

    config: enhance build

---
+++ config/visualization.plugin.ts
@@ -0,0 +1,19 @@
+import { visualizer } from 'rollup-plugin-visualizer'
+import templates, { type TemplateType } from 'rollup-plugin-visualizer/dist/plugin/template-types'
+
+const visualisationType = process.env.VISUALIZER || ''
+const isTemplateType = (value: string): value is TemplateType =>
+  (templates as unknown as { default: TemplateType[] }).default.map(String).includes(value)
+
+// use template types as VISUALIZER env variables to show bundle stats
+export const visualizationPlugin = () =>
+  isTemplateType(visualisationType)
+    ? {
+        ...visualizer({
+          gzipSize: true,
+          open: true,
+          template: visualisationType,
+          filename: 'build/bundle-stats.html',
+        }),
+      }
+    : undefined
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import { visualizationPlugin } from './config/visualization.plugin.ts'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths(), visualizationPlugin()],
   css: {
     postcss: './postcss.config.cjs',
   },
